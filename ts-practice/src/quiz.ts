f7cb8ebecf98879383422f41e42b0d90d0080218
JuniorTechie-coder <jtanmay787@gmail.com>
2026-09-10 20:41:52 +0530

    Rename Trello Clone to FlowDesk

---
+++ ts-practice/src/quiz.ts
@@ -0,0 +1,15 @@
+interface Question {
+    question: string,
+    answer: string
+}
+
+interface Players {
+    name: string,
+    score: number
+}
+
+const question: Question[] = [
+        "How are you",
+        "Tell me about yourself",
+         Why should we hire you
+    ];
